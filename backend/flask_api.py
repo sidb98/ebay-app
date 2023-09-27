60de576ede8237f2f45f9d4de940b1038259ee34
sidb98 <siddharth.byale@gmail.com>
2023-09-26 22:34:37 -0700

    first commit

---
+++ backend/flask_api.py
@@ -0,0 +1,265 @@
+from flask import Flask, request, current_app
+import dotenv
+import os
+import requests
+import json
+
+from collections import defaultdict
+from backend.ebay_oauth_token import OAuthToken
+
+
+app = Flask(__name__, static_url_path="", static_folder="../frontend/static")
+app.json.sort_keys = False
+
+dotenv.load_dotenv(dotenv.find_dotenv())
+
+
+def createPayload(data):
+    keyword = data["keyword"]
+    sort = data["sortBy"]
+
+    # TODO: sometimes i only get lesser than 10 items than the total fetched items
+    # https://piazza.com/class/lkyn4sr3nlj3/post/149
+    payload = {
+        "OPERATION-NAME": "findItemsByKeywords",
+        "SERVICE-VERSION": "1.0.0",
+        "SECURITY-APPNAME": os.getenv("APP_ID"),
+        "RESPONSE-DATA-FORMAT": "JSON",
+        "REST-PAYLOAD": "true",
+        # "paginationInput.entriesPerPage": "100",
+        "keywords": keyword,
+        "sortOrder": sort,
+    }
+
+    item_filter_idx = 0
+
+    # from_price = data["priceRange"]["from"]
+    # to_price = data["priceRange"]["to"]
+
+    from_price = data["fromPrice"]
+    to_price = data["toPrice"]
+    condition = data["conditions"]
+    seller = data["seller"]
+    shipping = data["shipping"]
+
+    if from_price:
+        payload[f"itemFilter({item_filter_idx}).name"] = "MinPrice"
+        payload[f"itemFilter({item_filter_idx}).value"] = from_price
+        payload[f"itemFilter({item_filter_idx}).paramName"] = "Currency"
+        payload[f"itemFilter({item_filter_idx}).paramValue"] = "USD"
+        item_filter_idx += 1
+
+    if to_price:
+        payload[f"itemFilter({item_filter_idx}).name"] = "MaxPrice"
+        payload[f"itemFilter({item_filter_idx}).value"] = to_price
+        payload[f"itemFilter({item_filter_idx}).paramName"] = "Currency"
+        payload[f"itemFilter({item_filter_idx}).paramValue"] = "USD"
+        item_filter_idx += 1
+
+    if condition:
+        payload[f"itemFilter({item_filter_idx}).name"] = "Condition"
+        for val in condition:
+            payload[
+                f"itemFilter({item_filter_idx}).value({condition.index(val)})"
+            ] = val
+        item_filter_idx += 1
+
+    if seller:
+        payload[f"itemFilter({item_filter_idx}).name"] = "ReturnsAcceptedOnly"
+        payload[f"itemFilter({item_filter_idx}).value"] = True
+        item_filter_idx += 1
+
+    if shipping:
+        if "FreeShippingOnly" in shipping:
+            payload[f"itemFilter({item_filter_idx}).name"] = "FreeShippingOnly"
+            payload[f"itemFilter({item_filter_idx}).value"] = True
+            item_filter_idx += 1
+        if "ExpeditedShippingType" in shipping:
+            payload[f"itemFilter({item_filter_idx}).name"] = "ExpeditedShippingType"
+            payload[f"itemFilter({item_filter_idx}).value"] = "Expedited"
+            item_filter_idx += 1
+
+    print("=" * 100)
+    print("Payload:", payload)
+    print("=" * 100)
+
+    with open("payload.json", "w") as f:
+        json.dump(payload, f)
+
+    return payload
+
+
+def cleanAllItemData(json_data):
+    if json_data["findItemsByKeywordsResponse"][0]["ack"][0] == "Failure":
+        return {"ack": "Failure"}
+    else:
+        if (
+            json_data["findItemsByKeywordsResponse"][0]["searchResult"][0]["@count"]
+            == "0"
+        ):
+            return {"ack": "Failure"}
+
+        else:
+            clean_json_data = {"ack": "Success", "total_fetched_items": 0, "items": []}
+
+            fetched_items = json_data["findItemsByKeywordsResponse"][0]["searchResult"][
+                0
+            ]["item"]
+            clean_json_data["total_fetched_items"] = json_data[
+                "findItemsByKeywordsResponse"
+            ][0]["paginationOutput"][0]["totalEntries"][0]
+
+            for idx, item in enumerate(fetched_items):
+                cleaned_item = {}
+                cleaned_item["itemId"] = item["itemId"][0]
+                cleaned_item["title"] = item["title"][0]
+                cleaned_item["category"] = item["primaryCategory"][0]["categoryName"][0]
+
+                condition_display_name = item.get("condition", [{}])[0].get(
+                    "conditionDisplayName", "N/A"
+                )
+                cleaned_item["condition"] = condition_display_name
+
+                cleaned_item["price"] = float(
+                    item["sellingStatus"][0]["convertedCurrentPrice"][0]["__value__"]
+                )
+
+                shipping_cost = float(
+                    item.get("shippingInfo", [{}])[0].get(
+                        "shippingServiceCost", [{"__value__": 0.0}]
+                    )[0]["__value__"]
+                )
+                cleaned_item["shipping"] = shipping_cost
+
+                cleaned_item["topRatedListing"] = bool(item["topRatedListing"][0])
+                cleaned_item["link"] = item["viewItemURL"][0]
+                cleaned_item["galleryURL"] = item["galleryURL"][0]
+
+                clean_json_data["items"].append(cleaned_item)
+
+                if idx == 9:
+                    print("=" * 100)
+                    print("Items fetched:", idx + 1)
+                    print("=" * 100)
+                    break
+
+    # with open("cleaned-data-all-items.json", "w") as f:
+    #     json.dump(clean_json_data, f)
+
+    return clean_json_data
+
+
+def cleanSingleItemData(json_data):
+    clean_json_data = defaultdict(str)
+    item_data = json_data["Item"]
+
+    clean_json_data["Photo"] = item_data.get("PictureURL", ["res/ebay_default.jpg"])[0]
+    clean_json_data["eBay Link"] = item_data.get("ViewItemURLForNaturalSearch", "")
+    clean_json_data["Title"] = item_data.get("Title", "")
+    clean_json_data["Subtitle"] = item_data.get("Subtitle", "")
+    clean_json_data["Price"] = str(item_data["CurrentPrice"].get("Value", 0.0)) + " USD"
+    clean_json_data["Location"] = (
+        item_data.get("Location", "") + " " + item_data.get("PostalCode", "")
+    )
+    clean_json_data["Seller"] = item_data.get("Seller", {}).get("UserID", "")
+
+    clean_json_data["Return Policy(US)"] = (
+        item_data.get("ReturnPolicy", {}).get("ReturnsAccepted", "")
+        + " within "
+        + item_data.get("ReturnPolicy", {}).get("ReturnsWithin", "")
+    )
+
+    # clean_json_data["itemSpecs"] = defaultdict(str)
+    for spec in item_data.get("ItemSpecifics", {}).get("NameValueList", []):
+        clean_json_data[spec.get("Name", "")] = spec.get("Value", [""])[0]
+
+    # with open("cleaned-data-single-item.json", "w") as f:
+        # json.dump(clean_json_data, f)
+
+    return clean_json_data
+
+
+@app.route("/", methods=["GET"])
+def index():
+    print("Sending index.html to client")
+    return current_app.send_static_file("index.html")
+
+
+@app.route("/findAllItems", methods=["GET"])
+def get_all_item():
+    try:
+        data = request.args.get("json")
+        data = json.loads(data)
+        print("=" * 100)
+        print("Received JSON data:", data)
+        print("=" * 100)
+
+    except json.JSONDecodeError as e:
+        print("Invalid JSON data")
+
+    api_url = "https://svcs.ebay.com/services/search/FindingService/v1"
+    payload = createPayload(data)
+    print("Sending request to eBay API for all items")
+    response = requests.get(api_url, params=payload)
+
+    if response.status_code == 200:
+        print(f"Sending response to client {response.status_code}")
+        print("=" * 100)
+
+        # saving response to json file
+        # with open("respone-data-all-items.json", "w") as f:
+        #     json.dump(response.json(), f)
+
+        clean_json = cleanAllItemData(response.json())
+        return clean_json
+    else:
+        return f"API request failed for all items {response.status_code}"
+
+
+@app.route("/getSingleItem", methods=["GET"])
+def get_single_item():
+    oauth_utility = OAuthToken(os.getenv("APP_ID"), os.getenv("CERT_ID"))
+    application_token = oauth_utility.getApplicationToken()
+
+    try:
+        item_id = request.args.get("itemId")
+        print("=" * 100)
+        print("Received Item ID:", item_id)
+        print("=" * 100)
+
+    except json.JSONDecodeError as e:
+        print("Invalid Item ID")
+
+    api_url = "https://open.api.ebay.com/shopping"
+
+    headers = {"X-EBAY-API-IAF-TOKEN": application_token}
+    payload = {
+        "callname": "GetSingleItem",
+        "responseencoding": "JSON",
+        "appid": os.getenv("APP_ID"),
+        "siteid": "0",
+        "version": "967",
+        "ItemID": item_id,
+        "IncludeSelector": "Description,Details,ItemSpecifics",
+    }
+
+    print("Sending request to eBay API for single item")
+    response = requests.get(api_url, params=payload, headers=headers)
+
+    if response.status_code == 200:
+        print(f"Sending response to client {response.status_code}")
+        print("=" * 100)
+
+        # saving response to json file
+        # with open("respone-data-single-item.json", "w") as f:
+        #     json.dump(response.json(), f)
+
+        clean_json = cleanSingleItemData(response.json())
+        return clean_json
+
+    else:
+        return f"API request failed for single item {response.status_code}"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ frontend/static/validation.js
@@ -0,0 +1,416 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    document.getElementById("clear").addEventListener("click", (event) => {
+        clearSearch();
+    });
+
+
+
+    document.getElementById("submit").addEventListener("click", (event) => {
+        event.preventDefault();
+        handleFormSubmit();
+    });
+
+    document.getElementById("back-to-search").addEventListener("click", (event) => {
+        event.preventDefault();
+        const backToSearchButton = document.getElementById("back-to-search");
+        backToSearchButton.style.display = 'none';
+
+        const line = document.getElementById("line");
+        line.style.display = '';
+
+        const itemResults = document.getElementById("item-results");
+        itemResults.style.display = '';
+
+        const toggleButton = document.getElementById("toggle-button");
+        toggleButton.style.display = '';
+
+        const singleItemResults = document.getElementById("single-item-results");
+        singleItemResults.style.display = 'none';
+
+        const itemDetailsLabel = document.getElementById("item-details-label");
+        itemDetailsLabel.style.display = 'none';
+
+        noResults = document.getElementById("no-results");
+        noResults.style.display = '';
+    });
+
+    document.getElementById("toggle-button").addEventListener("click", (event) => {
+        event.preventDefault();
+        const toggleButton = document.getElementById('toggle-button');
+        const items = document.querySelectorAll('.item');
+
+        if (toggleButton.value == 'Show More') {
+            items.forEach(item => {
+                item.style.display = '';
+            }
+            );
+            toggleButton.value = 'Show Less';
+            console.log("Toggle button showing more items");
+
+        }
+        else {
+            items.forEach((item, index) => {
+                if (index > 2) item.style.display = 'none';
+            }
+            );
+            toggleButton.value = 'Show More';
+            console.log("Toggle button showing less items");
+        }
+
+
+    });
+});
+
+
+function displaySearch() {
+    document.getElementById("no-results").style.display = "block";
+    document.getElementById("line").style.display = "block";
+    document.getElementById("item-results").style.display = "block";
+    document.getElementById("toggle-button").value = "Show More";
+    document.getElementById("toggle-button").style.display = "block";
+
+}
+
+
+function clearSearch() {
+    document.getElementById("no-results").innerHTML = "";
+    document.getElementById("item-results").innerHTML = "";
+    document.getElementById("toggle-button").value = "Show More";
+    document.getElementById("toggle-button").style.display = "none";
+    document.getElementById("back-to-search").style.display = "none";
+    document.getElementById("single-item-results").style.display = "none";
+    document.getElementById("item-details-label").style.display = "none";
+    document.getElementById("line").style.display = "none";
+}
+
+function validateForm() {
+
+    if (!document.getElementById("keyword").reportValidity()) {
+        console.log("Form validation failed");
+        return false;
+    }
+
+
+    const fromValue = parseFloat(document.getElementById("from").value);
+    const toValue = parseFloat(document.getElementById("to").value);
+
+    if (fromValue > toValue) {
+        alert("Oops! Lower price limit cannot be greater than upper price limit.\nPlease try again.");
+        console.log("Form validation failed");
+        return false;
+    }
+
+    if (fromValue < 0 || toValue < 0) {
+        alert("Price Range values cannot be negative.Please try a value greater than or equal to 0.0");
+        console.log("Form validation failed");
+        return false;
+    }
+    console.log("Form validated successfully");
+    return true;
+}
+
+function buildJsonData() {
+
+    // "how to put all the selected options from this form into a json object?" prompt. ChatGPT3.5, 11 Sep ChatGPT August 3 Version
+
+    var jsonObject = {};
+
+    jsonObject.keyword = document.getElementById("keyword").value;
+
+    // var priceRange = {
+    //     from: document.getElementById("from").value,
+    //     to: document.getElementById("to").value
+    // }
+
+    // jsonObject.priceRange = priceRange;
+
+
+    jsonObject.fromPrice = document.getElementById("from").value;
+    jsonObject.toPrice = document.getElementById("to").value;
+
+    var conditions = [];
+    if (document.getElementById("new").checked) conditions.push("1000");
+    if (document.getElementById("used").checked) conditions.push("3000");
+    if (document.getElementById("verygood").checked) conditions.push("4000");
+    if (document.getElementById("good").checked) conditions.push("5000");
+    if (document.getElementById("acceptable").checked) conditions.push("6000");
+    jsonObject.conditions = conditions;
+
+
+    if (!document.getElementById("return").checked) jsonObject.seller = "";
+    if (document.getElementById("return").checked) jsonObject.seller = "Return Accepted";
+
+    var shipping = [];
+    if (document.getElementById("free").checked) shipping.push("FreeShippingOnly");
+
+    // Uncomment the below line to enable expedited shipping
+    // if (document.getElementById("expedited").checked) shipping.push("ExpeditedShippingType");
+    jsonObject.shipping = shipping;
+
+    jsonObject.sortBy = document.getElementById("sortby").value;
+
+    return jsonObject;
+}
+
+
+function jsonToQueryString(json) {
+    const params = [];
+    for (const key in json) {
+      if (json[key] instanceof Array) {
+        // Handle arrays by joining their values with commas
+        params.push(`${key}=${json[key].join(',')}`);
+      } else {
+        params.push(`${key}=${json[key]}`);
+      }
+    }
+    return params.join('&');
+  }
+  
+
+
+function sendSearchDataToBackend(jsonObject) {
+
+    jsonData = JSON.stringify(jsonObject);
+    // console.log("New Json Data: " + jsonData)
+
+    const searchQuery = jsonToQueryString(jsonObject);
+    console.log("Search Query: " + searchQuery);
+
+    const url = `/findAllItems?json=${encodeURIComponent(jsonData)}`;
+
+
+    const options = {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    };
+
+    fetch(url, options)
+        .then(response => {
+            if (response.ok) {
+                console.log('Search parameters sent successfully to Flask');
+                return response.json();
+            } else {
+                console.error('Error sending data to Flask: ' + response.status);
+            }
+        })
+        .then(data => {
+            reponseData = data;
+            console.log('JSON response from Flask for all items:');
+            console.log(reponseData)
+            populateResults(reponseData);
+        })
+        .catch(error => {
+            console.error('Network error:', error);
+        });
+
+}
+
+function handleFormSubmit() {
+    if (validateForm()) {
+        var jsonData = buildJsonData();
+        sendSearchDataToBackend(jsonData);
+        return true;
+    }
+
+    return false;
+}
+
+function allItemsTemplate(item) {
+    const itemContainer = document.createElement('div');
+    itemContainer.classList.add('item');
+
+    const imageContainer = document.createElement('div');
+    imageContainer.classList.add('image-container');
+
+    const image = document.createElement('img');
+    image.src = item.galleryURL;
+
+    const textElement = document.createElement('div');
+    textElement.classList.add('text-container');
+
+    const title = document.createElement('h3');
+    title.textContent = item.title;
+
+    const category = document.createElement('p');
+    category.classList.add('category');
+    category.innerHTML = `Category: <em>${item.category}</em>`;
+
+    const redirectImage = document.createElement('img');
+    redirectImage.classList.add('redirect-image');
+
+    const condition = document.createElement('p');
+    condition.textContent = `Condition: ${item.condition}`;
+
+    if (item.topRatedListing) {
+        const topRatedImage = document.createElement('img');
+        topRatedImage.classList.add('top-rated-image');
+        condition.appendChild(topRatedImage);
+    }
+
+    const price = document.createElement('p');
+    price.style.fontWeight = 'bold';
+    if (item.shipping != 0)
+        price.textContent = `Price: $${item.price} (+ $${item.shipping} for shipping)`;
+    else
+        price.textContent = `Price: $${item.price}`;
+
+    category.appendChild(redirectImage);
+
+    textElement.appendChild(title);
+    textElement.appendChild(category);
+    textElement.appendChild(condition);
+    textElement.appendChild(price);
+
+
+    imageContainer.appendChild(image);
+
+    itemContainer.appendChild(imageContainer);
+    itemContainer.appendChild(textElement);
+
+    itemContainer.onclick = () => {
+        sendSingleItemToBackend(item);
+    };
+
+    return itemContainer;
+}
+
+function populateResults(responseData) {
+
+    clearSearch();
+    const noResults = document.getElementById("no-results");
+    const keyword = document.getElementById("keyword").value;
+
+    if (responseData.ack == "Failure") {
+        noResults.style.display = '';
+        noResults.textContent = `No results found`;
+        return;
+    }
+
+    noResults.style.display = ''
+    noResults.innerHTML = `${responseData.total_fetched_items} results found for <em>${keyword}</em>`;
+
+
+    const parentContainer = document.getElementById("item-results")
+
+    itemList = responseData.items
+
+    itemList.forEach((element, index) => {
+        const item = allItemsTemplate(element);
+
+        if (index < 3) item.style.display = '';
+        else item.style.display = 'none';
+
+        parentContainer.appendChild(item);
+
+    });
+    console.log("Results populated successfully for all items");
+
+    const toggleButton = document.getElementById('toggle-button');
+    toggleButton.style.display = '';
+
+    displaySearch();
+}
+
+function populateSingleItem(responseData) {
+
+    backToSearchButton = document.getElementById("back-to-search");
+    backToSearchButton.style.display = '';
+
+    itemResults = document.getElementById("item-results");
+    itemResults.style.display = 'none';
+
+    line = document.getElementById("line");
+    line.style.display = 'none';
+
+    toggleButton = document.getElementById("toggle-button");
+    toggleButton.style.display = 'none';
+
+    itemDetailsLabel = document.getElementById("item-details-label");
+    itemDetailsLabel.style.display = '';
+
+    noResults = document.getElementById("no-results");
+    noResults.style.display = 'none';
+
+    const parentContainer = document.getElementById("single-item-results")
+    parentContainer.style.display = '';
+    parentContainer.innerHTML = "";
+
+    const tableContainer = document.createElement('table');
+    tableContainer.classList.add('table');
+
+
+
+    Object.keys(responseData).forEach(key => {
+
+        if (responseData[key] != "") {
+
+            const row = document.createElement('tr');
+            const labelCell = document.createElement('th');
+            labelCell.textContent = key;
+
+            const valueCell = document.createElement('td');
+
+            if (key == "Photo") {
+                const image = document.createElement('img');
+                image.src = responseData[key];
+                image.width = 200;
+                image.height = 200;
+                valueCell.appendChild(image);
+            }
+            else if (key == "eBay Link") {
+                const link = document.createElement('a');
+                link.href = responseData[key];
+                link.textContent = "eBay Product Link";
+                link.target = "_blank";
+                valueCell.appendChild(link);
+            }
+            else
+                valueCell.textContent = responseData[key];
+
+            row.appendChild(labelCell);
+            row.appendChild(valueCell);
+
+            tableContainer.appendChild(row);
+        }
+    });
+
+    parentContainer.appendChild(tableContainer);
+
+    console.log("Results populated successfully for single item");
+
+}
+
+function sendSingleItemToBackend(item) {
+
+    const url = `/getSingleItem?itemId=${(item.itemId)}`;
+
+    const options = {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    };
+
+    fetch(url, options)
+        .then(response => {
+            if (response.ok) {
+                console.log('Single Item data sent successfully to Flask');
+                return response.json();
+            } else {
+                console.error('Error sending data to Flask: ' + response.status);
+            }
+        })
+        .then(data => {
+            reponseData = data;
+            console.log('JSON response from Flask for single item:');
+            console.log(reponseData)
+            populateSingleItem(reponseData);
+        })
+        .catch(error => {
+            console.error('Network error:', error);
+        });
+}
+
